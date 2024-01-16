51061f1a94ba96ac0d871226ecb66beae1e7fc13
Adrian Hernandez <76405864+AdrianHernandezll@users.noreply.github.com>
2024-01-16 01:52:47 -0300

    agregando hook y eventos en formulario

--- src/component/Formulario.jsx
+++ src/component/Formulario.jsx
@@ -1,6 +1,18 @@
+import { useState,useEffect } from "react";
+
 const Formulario = () => {
+
+  const [nombre,setNombre] = useState('');
+  const [propietario,setPropietario] = useState('');
+  const [email,setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas,setSintomas] = useState('');
+
+
+
+
   return (
-    <div className="w-1/ lg:w-3/5">
+    <div className="w-1/ lg:w-3/5 mx-5">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
         <p className="text-lg mt-5 text-center mb-10">
@@ -15,6 +27,8 @@ const Formulario = () => {
               type="text" 
               placeholder="Nombre de la Mascota"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={nombre}
+              onChange={(e)=> setNombre(e.target.value)}
               />
           </div>
 
@@ -25,6 +39,8 @@ const Formulario = () => {
               type="text" 
               placeholder="Nombre del Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={propietario}
+              onChange={(e)=> setPropietario(e.target.value)}
               />
           </div>
           <div className="mb-5">
@@ -34,6 +50,8 @@ const Formulario = () => {
               type="text" 
               placeholder="Email Contacto Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
               />
           </div>
 
@@ -43,21 +61,24 @@ const Formulario = () => {
               id="alta"
               type="time" 
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={fecha}
+              onChange={(e)=> setFecha(e.target.value)}
               />
           </div>
           <div className="mb-5">
               <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
               <textarea
               id="sintomas"
-    
               className="border-2 w-full mt-2 placeholder-gray-400 rounded-md"
               placeholder="Describe los sintomas"
+              value={sintomas}
+              onChange={(e)=> setSintomas(e.target.value)}
               />
           </div>
           
           <input
             type="submit"
-            className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all transition-colors"
+            className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
             value="Agrega Paciente"
           />
 

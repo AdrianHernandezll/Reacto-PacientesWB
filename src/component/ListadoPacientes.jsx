2ef688c058964cd1d421f9b3082772da17e52c1c
Adrian Hernandez <76405864+AdrianHernandezll@users.noreply.github.com>
2024-01-16 01:17:47 -0300

    Probando importancion nuevo componente

--- src/component/ListadoPacientes.jsx
+++ src/component/ListadoPacientes.jsx
@@ -1,14 +1,19 @@
+import Pacientes from "./Pacientes";
+
 const ListadoPacientes = () => {
 
   return(
-    <div className="w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h1 className="font-black text-3xl text-center">Listado de Pacientes</h1>
       <p className="text-xl mt-5 mb-10 text-center">
         Administra tus {' '}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
 
-      
+      <Pacientes/>
+      <Pacientes/>
+      <Pacientes/>
+      <Pacientes/>
       
     </div>
   )

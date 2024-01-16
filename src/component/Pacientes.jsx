35805880ac11f255fd7428be558af59ae578f956
Adrian Hernandez <76405864+AdrianHernandezll@users.noreply.github.com>
2024-01-16 01:12:34 -0300

    Creando componente separado para pacientes

---
+++ src/component/Pacientes.jsx
@@ -0,0 +1,27 @@
+
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {' '}
+            <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {' '}
+            <span className="font-normal normal-case">Daniel</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {' '}
+            <span className="font-normal normal-case ">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha: {' '}
+            <span className="font-normal normal-case ">10 de Diciembre 2020</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Alta de Paciente: {' '}
+            <span className="font-normal normal-case ">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {' '}
+            <span className="font-normal normal-case ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis molestiae eius. Error aut totam consectetur necessitatibus ex at magnam quibusdam, doloremque unde, illum repellendus omnis ullam. Alias, dignissimos nulla.</span>
+        </p>
+      </div>
+  )
+}
+
+export default Pacientes

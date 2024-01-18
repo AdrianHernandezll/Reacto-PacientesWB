262cece44fdb12ec03b6a98bb8c6c959770dc0f6
Adrian Hernandez <76405864+AdrianHernandezll@users.noreply.github.com>
2024-01-18 00:32:50 -0300

    Agregando los primeros props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Formulario from "./component/Formulario";
 import Header from "./component/Header";
 import ListadoPacientes from "./component/ListadoPacientes";
 
 
 function App() {
+
+  const [pacientes,setPacientes] = useState([]);
+
+
   return(
 
     <div className="container mx-auto mt-20">
       <Header/>
       <div className="mt-12 md:flex">
-        <Formulario/> 
+        <Formulario
+         pacientes={pacientes} 
+         setPacientes={setPacientes}
+         /> 
         <ListadoPacientes/>
       </div>
   
